571ea1b2c5b816239d6c42ecdee0f1cc98b92b07
Duk1125 <jinjax1125@gmail.com>
2026-04-16 20:22:26 +0800

    first upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://jkszpdiipggrfeolzlyh.supabase.co';
+const supabaseKey = 'sb_publishable_rgOp521RQOCWZwF8ZuvLrA_Qy-ilY-8';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
